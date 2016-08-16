91fbe7cce6bd1b2d43c7f4c4e2197d9ced4f82ce
sasitornamy <sasitorn.oeam@gmail.com>
2016-08-16 13:03:24 +0700

    Added submit button on thank-you page (#5)

--- app/models/stimulus.rb
+++ app/models/stimulus.rb
@@ -4,4 +4,11 @@ class Stimulus < ActiveRecord::Base
 
   scope :random, -> { order('RANDOM()') }
   scope :excluded_id, -> (ids) { where.not(id: ids) }
+
+  def self.next_remaining(experiment_session_id, trial_order)
+    return unless trial_order.to_i < ENV['NUMBER_OF_TRIALS'].to_i
+    current_session = ExperimentSession.find(experiment_session_id)
+    completed_stimulus_ids = current_session.stimuli.pluck(:id)
+    Stimulus.excluded_id(completed_stimulus_ids).random.first
+  end
 end
